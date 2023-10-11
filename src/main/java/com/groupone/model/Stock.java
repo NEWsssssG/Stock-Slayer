3b018cbdd92bc1ca94670b510b4fb4b0f900c83c
NEWGSSSSS <143850273+NEWsssssG@users.noreply.github.com>
2023-10-10 22:22:44 -0500

    Update Stock.java
    Made fields private.
    
    Reduced the printStockData method since the toString method already provides that functionality.

--- src/main/java/com/groupone/model/Stock.java
+++ src/main/java/com/groupone/model/Stock.java
@@ -1,25 +1,18 @@
 package com.groupone.model;
 
 /**
- * Abstracts and stores information about a stock that a user has purchased such as
- * the unique stock ID (transaction), the owner ID of the one who purchased the stock,
- * the symbol, the volume, and the value.
+ * Represents a stock that a user has purchased.
+ * Contains information like the unique stock ID (transaction), the owner ID, 
+ * the stock symbol, volume, and value of the purchase.
  */
 public class Stock {
-	int id;		// Unique ID integer given to every stock transaction
-	int ownerID;		// Unique ID integer linked to the user id, the one who purchased the stock
-	String symbol;		// String symbol of the stock, such as "IBM" or "APPL"
-	double volume;		// Double volume, or number of shares purchased
-	double value;		// Double value, value of each share that was purchased
-	
-	/**
-	 * Creates Stock object with all data filled out
-	 * @param id Unique ID integer given to every stock transaction
-	 * @param ownerID Unique ID integer linked to the user id, the one who purchased the stock
-	 * @param symbol String symbol of the stock, such as "IBM" or "APPL"
-	 * @param volume Double volume, or number of shares purchased
-	 * @param value Double value, value of each share that was purchased
-	 */
+
+	private int id;		// Unique ID for every stock transaction
+	private int ownerID;	// ID linked to the user who purchased the stock
+	private String symbol;	// Symbol of the stock, e.g., "IBM" or "AAPL"
+	private double volume;	// Number of shares purchased
+	private double value;	// Value of each share purchased
+
 	public Stock(int id, int ownerID, String symbol, double volume, double value) {
 		this.id = id;
 		this.ownerID = ownerID;
@@ -35,81 +28,36 @@ public class Stock {
 		this.value = value;
 	}
 
-	/**
-	 * Creates a partly made Stock object with only symbol, volume, and value filled out. No associated user or stock id.
-	 * @param symbol String symbol of the stock, such as "IBM" or "APPL"
-	 * @param volume Double volume, or number of shares purchased
-	 * @param value Double value, value of each share that was purchased
-	 */
 	public Stock(String symbol, double volume, double value) {
 		this.symbol = symbol;
 		this.volume = volume;
 		this.value = value;
 	}
-	
-	/**
-	 * Prints ALL the data in the object
-	 */
-	public void printStockData() {
-		System.out.println("stockID : " + this.id);
-		System.out.println("ownerID : " + this.ownerID);
-		System.out.println("symbol : " + this.symbol);
-		System.out.println("volume : " + this.volume);
-		System.out.println("value : " + this.value);
-	}
-	
-	/**
-	 * Returns the integer stockID attribute
-	 * @return integer stockID
-	 */
+
 	public int getId() {
 		return this.id;
 	}
-	
-	/**
-	 * Sets the integer stockID attribute
-	 * @param id Integer stockID
-	 */
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	/**
-	 * Returns the integer ownerID attribute
-	 * @return integer ownerID
-	 */
+
 	public int getOwnerId() {
 		return this.ownerID;
 	}
-	
-	/**
-	 * Sets the integer ownerID attribute
-	 * @param ownerID Integer
-	 */
+
 	public void setOwnerId(int ownerID) {
 		this.ownerID = ownerID;
 	}
-	
-	/**
-	 * Returns the String symbol attribute
-	 * @return String symbol
-	 */
-	public String getSymbol(){
+
+	public String getSymbol() {
 		return this.symbol;
 	}
-	
-	/**
-	 * Returns the double volume attribute
-	 * @return Double volume
-	 */
+
 	public double getVolume() {
 		return this.volume;
 	}
-	
-	/**
-	 * Returns the double value attribute
-	 * @return Double value
-	 */
+
 	public double getValue() {
 		return this.value;
 	}
