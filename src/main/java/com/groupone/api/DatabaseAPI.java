f14705effc2cf8925b4ea3c8886159539e9a4f50
NEWGSSSSS <143850273+NEWsssssG@users.noreply.github.com>
2023-10-10 23:23:18 -0500

    Update DatabaseAPI.java
    Refactor adopted prepared statements in SQL for better safety and readability.
    
    Better Consistency standardized use of `jdbcTemplate` methods across the board.
     
    Reduced code repetition using overloaded methods.
    
    Optimization for Direct SQL user lookup for efficiency rather than iterating all users.
    
    Security for Shifted to parameterized SQL queries from string formatting to prevent potential SQL injection.
    
    Clarity Enhanced console output messages for improved debugging.

--- src/main/java/com/groupone/api/DatabaseAPI.java
+++ src/main/java/com/groupone/api/DatabaseAPI.java
@@ -2,17 +2,13 @@ package com.groupone.api;
 
 import com.groupone.model.Stock;
 import com.groupone.model.User;
-import com.groupone.service.DatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
-import java.sql.Types;
-import java.util.List;
-import java.util.Objects;
 
 @Component
 public class DatabaseAPI implements CommandLineRunner {
@@ -20,45 +16,15 @@ public class DatabaseAPI implements CommandLineRunner {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void initDatabase(){
-        jdbcTemplate.execute(
-                "CREATE TABLE IF NOT EXISTS user(" +
-                "id             INTEGER PRIMARY KEY," +
-                "email          STRING NOT NULL," +
-                "password       STRING NOT NULL," +
-                "availableFunds DOUBLE," +
-                "isLocked       BOOLEAN);");
+    public void initDatabase() {
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY, email STRING NOT NULL, password STRING NOT NULL, availableFunds DOUBLE, isLocked BOOLEAN);");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS stock(id INTEGER PRIMARY KEY, ownerId INTEGER NOT NULL REFERENCES user (id), symbol STRING NOT NULL, volume DOUBLE, value DOUBLE, FOREIGN KEY (ownerId) REFERENCES user (id));");
 
-        jdbcTemplate.execute(
-                "CREATE TABLE IF NOT EXISTS stock(" +
-                "id         INTEGER PRIMARY KEY," +
-                "ownerId    INTEGER NOT NULL REFERENCES user (id)," +
-                "symbol     STRING NOT NULL," +
-                "volume     DOUBLE," +
-                "value      DOUBLE," +
-                "FOREIGN KEY (ownerId) REFERENCES user (id));"
-        );
+        List<User> users = queryForUsers("SELECT * FROM user");
+        List<Stock> stocks = queryForStocks("SELECT * FROM stock");
+        pairUsersToStocks(users, stocks);
 
-        List<User> users = jdbcTemplate.query("SELECT * FROM user;",
-                (resultSet, rowNum) -> new User(
-                        resultSet.getInt("id"),
-                        resultSet.getString("email"),
-                        resultSet.getString("password"),
-                        resultSet.getBoolean("isLocked"),
-                        resultSet.getDouble("availableFunds")
-                ));
-
-        List<Stock> stocks = jdbcTemplate.query("SELECT * FROM stock;",
-                (resultSet, rowNum) -> new Stock(
-                        resultSet.getInt("id"),
-                        resultSet.getInt("ownerId"),
-                        resultSet.getString("symbol"),
-                        resultSet.getDouble("volume"),
-                        resultSet.getDouble("value")
-                ));
-        users = pairUsersToStocks(users, stocks);
-
-        if(!users.isEmpty() || !stocks.isEmpty()){
+        if (!users.isEmpty() || !stocks.isEmpty()) {
             users.forEach(System.out::println);
             stocks.forEach(System.out::println);
         } else {
@@ -66,155 +32,125 @@ public class DatabaseAPI implements CommandLineRunner {
         }
     }
 
-    public void addUserRecord(String email, String password) throws Exception{
-        if(getUserRecord(new User(email, password)) != null) throw new Exception("User already in database");
-
-        final String addSql = "INSERT INTO user (email, password) VALUES (?, ?)";
-        Object[] params = {email, password};
-        int[] types = {Types.VARCHAR, Types.VARCHAR};
-
-        int row = this.jdbcTemplate.update(addSql, params, types);
+    public void addUserRecord(String email, String password) throws Exception {
+        if (getUserByEmail(email) != null) throw new Exception("User already in database");
+        jdbcTemplate.update("INSERT INTO user (email, password) VALUES (?, ?)", email, password);
         System.out.printf("Added User: %s%n", email);
     }
 
-    public void addUserRecord(User user) throws Exception{
-        String userEmail = user.getEmail();
-        String userPassword = user.getPassword();
-        addUserRecord(userEmail, userPassword);
+    public void addUserRecord(User user) throws Exception {
+        addUserRecord(user.getEmail(), user.getPassword());
     }
 
-    public void deleteUserRecord(int userId){
+    public User getUserByEmail(String email) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM user WHERE email = ?", new Object[]{email}, (resultSet, rowNum) -> new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("email"),
+                    resultSet.getString("password"),
+                    resultSet.getBoolean("isLocked"),
+                    resultSet.getDouble("availableFunds")));
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public void deleteUserRecord(int userId) {
         final String deleteSql = "DELETE FROM user WHERE id=?";
-        Object[] params = {userId};
-        int[] types = {Types.INTEGER};
-        int rows = this.jdbcTemplate.update(deleteSql, params, types);
+        jdbcTemplate.update(deleteSql, userId);
         deleteUserStocks(userId);
         System.out.printf("Deleted User: %s%n", userId);
     }
 
-    public User getUserRecord(User user){
-        User foundUser = null;
-        List<User> recordedUsers = getUserTableInfo();
-        for(User recordedUser : recordedUsers){
-            if(Objects.equals(recordedUser.getEmail(), user.getEmail())) {
-                foundUser = recordedUser;
-                break;
-            }
-        }
-
-        return foundUser;
-    }
-
-    public void updateUserRecord(User oldUser, User newUser){
-        final String updateSql = "UPDATE user (email, password, isLocked) SET(?, ?, ?) WHERE id = ?";
-        Object[] params = {newUser.getEmail(), newUser.getPassword(), newUser.getIsLocked(), oldUser.getId()};
-        Object[] types = {Types.VARCHAR, Types.VARCHAR, Types.BOOLEAN, Types.INTEGER};
-        int rows = this.jdbcTemplate.update(updateSql, params, types);
+    public void updateUserRecord(User oldUser, User newUser) {
+        final String updateSql = "UPDATE user SET email = ?, password = ?, isLocked = ? WHERE id = ?";
+        jdbcTemplate.update(updateSql, newUser.getEmail(), newUser.getPassword(), newUser.getIsLocked(), oldUser.getId());
         System.out.printf("Updated User: %1$s %n%2$s %n", oldUser.getId(), newUser);
     }
 
-    public List<User> pairUsersToStocks(List<User> users, List<Stock> stocks){
-        users.forEach(user -> {
-            stocks.forEach(stock -> {
-                if(user.getId() == stock.getOwnerId()){
-                    user.addStock(stock);
-                }
-            });
-        });
+    public List<User> pairUsersToStocks(List<User> users, List<Stock> stocks) {
+        users.forEach(user -> stocks.stream().filter(stock -> user.getId() == stock.getOwnerId()).forEach(user::addStock));
         return users;
     }
 
-    public void addStockRecord(int ownerId, String symbol, double volume, double value) throws Exception{
-        List<User> users = getUserTableInfo();
-        boolean isValidOwnedId = false;
-        for (User user: users) {
-            if(user.getId() == ownerId){
-                isValidOwnedId = true;
-                break;
-            }
-        }
-
-        if(!isValidOwnedId) throw new Exception("Not Valid Owner ID!");
-
-        final String addSql = String.format("INSERT INTO stock (ownerId, symbol, volume, value) VALUES ('%1$s', '%2$s', '%3$s', '%4$s');",
-                ownerId, symbol, volume, value);
-        this.jdbcTemplate.execute(addSql);
+    public void addStockRecord(int ownerId, String symbol, double volume, double value) throws Exception {
+        if (getUserById(ownerId) == null) throw new Exception("Not Valid Owner ID!");
+        jdbcTemplate.update("INSERT INTO stock (ownerId, symbol, volume, value) VALUES (?, ?, ?, ?)", ownerId, symbol, volume, value);
         System.out.printf("Added Stock to user: %s%n", ownerId);
     }
 
-    public void addStockRecord(Stock stock) throws Exception{
-        addStockRecord(stock.getOwnerId(),
-                stock.getSymbol(),
-                stock.getVolume(),
-                stock.getValue());
+    public void addStockRecord(Stock stock) throws Exception {
+        addStockRecord(stock.getOwnerId(), stock.getSymbol(), stock.getVolume(), stock.getValue());
     }
 
     public void deleteStockRecord(int stockId) {
-        jdbcTemplate.execute(String.format("DELETE FROM stock WHERE id='%s';", stockId));
+        jdbcTemplate.update("DELETE FROM stock WHERE id = ?", stockId);
         System.out.printf("Deleted Stock w/ id: %s%n", stockId);
     }
 
-    public List<User> getUserTableInfo(){
-        String selectSql = "SELECT * FROM user";
-        return queryForUsers(selectSql);
+    public List<User> getUserTableInfo() {
+        return queryForUsers("SELECT * FROM user");
     }
 
     public List<Stock> getStockTableInfo() {
-        String selectSql = "SELECT * FROM stock";
-        return queryForStocks(selectSql);
+        return queryForStocks("SELECT * FROM stock");
     }
 
-    public void addAvailableFunds(User user, double funds){
-        User userRecord = getUserRecord(user);
+    public void addAvailableFunds(User user, double funds) {
+        User userRecord = getUserByEmail(user.getEmail());
         userRecord.addFunds(funds);
-        jdbcTemplate.execute(String.format("UPDATE user SET availableFunds=%1$f WHERE id=%2$s;",
-                userRecord.getAvailableFunds(), userRecord.getId()));
+        jdbcTemplate.update("UPDATE user SET availableFunds = ? WHERE id = ?", userRecord.getAvailableFunds(), userRecord.getId());
         System.out.printf("Set availableFunds %1$s to user %2$s", userRecord.getAvailableFunds(), user.getEmail());
     }
 
-    public void subtractAvailableFunds(User user, double funds){
-        User userRecord = getUserRecord(user);
+    public void subtractAvailableFunds(User user, double funds) {
+        User userRecord = getUserByEmail(user.getEmail());
         userRecord.subtractFunds(funds);
-        jdbcTemplate.execute(String.format("UPDATE user SET availableFunds=%1$f WHERE id=%2$s;",
-                userRecord.getAvailableFunds(), userRecord.getId()));
+        jdbcTemplate.update("UPDATE user SET availableFunds = ? WHERE id = ?", userRecord.getAvailableFunds(), userRecord.getId());
         System.out.printf("Set availableFunds %1$s to user %2$s", userRecord.getAvailableFunds(), user.getEmail());
     }
 
-    public void toggleUserLocked(User user){
-        User userRecord = getUserRecord(user);
+    public void toggleUserLocked(User user) {
+        User userRecord = getUserByEmail(user.getEmail());
         userRecord.toggleLock();
-        jdbcTemplate.execute(String.format("UPDATE user SET isLocked=%1$s WHERE id='%2$s'",
-                userRecord.getIsLocked(), userRecord.getId()));
+        jdbcTemplate.update("UPDATE user SET isLocked = ? WHERE id = ?", userRecord.getIsLocked(), userRecord.getId());
         System.out.printf("isLocked set to: %1$s user account: %2$s", userRecord.getIsLocked(), userRecord.getEmail());
     }
 
-    private void deleteUserStocks(int userId){
-        jdbcTemplate.execute(String.format("DELETE FROM stock WHERE ownerId='%s';", userId));
-        System.out.printf("Deleted User: %s stocks%n", userId);
+    private User getUserById(int id) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM user WHERE id = ?", new Object[]{id}, (resultSet, rowNum) -> new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("email"),
+                    resultSet.getString("password"),
+                    resultSet.getBoolean("isLocked"),
+                    resultSet.getDouble("availableFunds")));
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
-    private List<User> queryForUsers(String query){
-        List<User> users = jdbcTemplate.query(query,
-                (resultSet, rowNum) -> new User(
-                        resultSet.getInt("id"),
-                        resultSet.getString("email"),
-                        resultSet.getString("password"),
-                        resultSet.getBoolean("isLocked"),
-                        resultSet.getDouble("availableFunds")
-                ));
-        users = pairUsersToStocks(users, getStockTableInfo());
-        return users;
+    private void deleteUserStocks(int userId) {
+        jdbcTemplate.update("DELETE FROM stock WHERE ownerId = ?", userId);
+        System.out.printf("Deleted stocks owned by User ID: %s%n", userId);
     }
 
-    private List<Stock> queryForStocks(String query){
-        return jdbcTemplate.query(query,
-                (resultSet, rowNum) -> new Stock(
-                        resultSet.getInt("id"),
-                        resultSet.getInt("ownerId"),
-                        resultSet.getString("symbol"),
-                        resultSet.getDouble("volume"),
-                        resultSet.getDouble("value")
-                ));
+    private List<User> queryForUsers(String query) {
+        return jdbcTemplate.query(query, (resultSet, rowNum) -> new User(
+                resultSet.getInt("id"),
+                resultSet.getString("email"),
+                resultSet.getString("password"),
+                resultSet.getBoolean("isLocked"),
+                resultSet.getDouble("availableFunds")));
+    }
+
+    private List<Stock> queryForStocks(String query) {
+        return jdbcTemplate.query(query, (resultSet, rowNum) -> new Stock(
+                resultSet.getInt("id"),
+                resultSet.getInt("ownerId"),
+                resultSet.getString("symbol"),
+                resultSet.getDouble("volume"),
+                resultSet.getDouble("value")));
     }
 
     @EventListener(ApplicationReadyEvent.class)
@@ -224,8 +160,7 @@ public class DatabaseAPI implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        // logic for CommandLineRunner (if any is intended)
     }
-
 }
-
+            
