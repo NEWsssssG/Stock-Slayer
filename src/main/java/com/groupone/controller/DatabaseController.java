c5b45c050d2c1d3a0ffa6f6efe5f82acd4a83631
NEWGSSSSS <143850273+NEWsssssG@users.noreply.github.com>
2023-10-10 22:20:49 -0500

    Update DatabaseController.java
    Added Common Path: I added a common @RequestMapping("/database") at the class level to avoid repetition.
    
    Replaced @RequestMapping: Replaced with @GetMapping and @PostMapping for clarity.
    
    Simplified Methods: The updateUser method was simplified. Ideally, your service layer should have more checks and error handling, so the controller remains clean.
    
    Error Handling: While the provided snippet doesn't show how DatabaseService methods are implemented, you should ensure that they handle potential errors properly. If they throw exceptions, you might want to handle those exceptions in the controller (or use an exception handler) to provide meaningful feedback to the user.

--- src/main/java/com/groupone/controller/DatabaseController.java
+++ src/main/java/com/groupone/controller/DatabaseController.java
@@ -1,8 +1,5 @@
 package com.groupone.controller;
 
-import com.groupone.api.DatabaseAPI;
-import com.groupone.model.Stock;
-import com.groupone.model.User;
 import com.groupone.service.DatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,53 +8,53 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
+@RequestMapping("/database")  // Move common path segment here for better structure
 public class DatabaseController {
+
     @Autowired
     DatabaseService databaseService;
 
     private final RedirectView defaultRedirection = new RedirectView("/database");
 
-    @RequestMapping("/database")
-    public String databaseIndex(Model model){
+    @GetMapping
+    public String databaseIndex(Model model) {
         model.addAttribute("userList", databaseService.getUserTableInfo());
         model.addAttribute("stockList", databaseService.getStockTableInfo());
         return "databaseIndex";
     }
 
-    @RequestMapping("/database/delete/user/{userId}")
-    public RedirectView deleteUser(@PathVariable int userId){
+    @GetMapping("/delete/user/{userId}")
+    public RedirectView deleteUser(@PathVariable int userId) {
         databaseService.deleteUserRecord(userId);
         return defaultRedirection;
     }
 
-    @RequestMapping("/database/delete/stock/{stockId}")
-    public RedirectView deleteStock(@PathVariable int stockId){
+    @GetMapping("/delete/stock/{stockId}")
+    public RedirectView deleteStock(@PathVariable int stockId) {
         databaseService.deleteStockRecord(stockId);
         return defaultRedirection;
     }
 
-//    @PostMapping("/database/update/")
-//    public RedirectView updateUser(@ModelAttribute User user){
-//        System.out.println(user);
-//        User foundUser = null;
-//        try{
-//            foundUser = databaseService.getUser(user.getID());
-//        } catch (Exception e) {
-//            System.err.println(e.getMessage());
-//        }
-//        if(foundUser!=null) databaseAPI.updateUserRecord(foundUser, user);
-//        return defaultRedirection;
-//    }
-
-    @PostMapping("/database/add/user")
-    public RedirectView addUser(@ModelAttribute User user){
-        databaseService.addUserRecord(user.getEmail(), user.getPassword());
+    @PostMapping("/update")
+    public RedirectView updateUser(@ModelAttribute User user) {
+        // I simplified the logic here. Ideally, your service layer should handle checks
+        if (user != null && user.getID() != null) {
+            databaseService.updateUserRecord(user);
+        }
         return defaultRedirection;
     }
 
-    @PostMapping("database/add/stock")
+    @PostMapping("/add/user")
+    public RedirectView addUser(@ModelAttribute User user) {
+        if (user != null) {
+            databaseService.addUserRecord(user.getEmail(), user.getPassword());
+        }
+        return defaultRedirection;
+    }
+
+    @PostMapping("/add/stock")
     public RedirectView addStock(@RequestParam int ownerId, @RequestParam String symbol,
-                                 @RequestParam Double volume, @RequestParam Double value){
+                                 @RequestParam Double volume, @RequestParam Double value) {
         databaseService.addStockRecord(ownerId, symbol, volume, value);
         return defaultRedirection;
     }
